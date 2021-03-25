e059331573062970e38cd533cb97d7533858d8bf
taotao_glb <taotao_glb@icloud.com>
2021-03-26 01:57:46 +0800

    render the robot list,but no css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import robots from "./mockdata/robots.json";
+import Robot from "./components/Robot";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {robots.map((r) => (
+          <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
+        ))}
+      </ul>
     </div>
   );
 }
