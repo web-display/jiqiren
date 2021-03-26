1065033d6ec418c33640abf031b7b5c5475ae77d
taotao_glb <taotao_glb@icloud.com>
2021-03-26 08:50:56 +0800

    font-face

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import Robot from "./components/Robot";
 
 function App() {
 	return (
-    <div className={style.app}>
-      <div className={style.robotList}>
+		<div className={style.app}>
+			<div className={style.appHeader}>
+				<img src={logo} alt="logo" className={style.appLogo} />
+				<h1>罗伯特的购物平台名字很长</h1>
+			</div>
+			<div className={style.robotList}>
 				{robots.map((r) => (
 					<Robot key={r.id} id={r.id} name={r.name} email={r.email} />
 				))}
-      </div>
-			
+			</div>
 		</div>
 	)
 }
