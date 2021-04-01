618813e76b6b38b0b9a0c9c95b76073d6a2a0d84
taotao_glb <taotao_glb@icloud.com>
2021-04-01 10:16:13 +0800

    fetch data use async/await

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,19 @@ const App: React.FC = (props) => {
 	useEffect(() => {
 		document.title = `点击次数${count}`
 	}, [count])
+	// 使用promise请求数据
+	// useEffect(() => {
+	// 	fetch('https://jsonplaceholder.typicode.com/users')
+	// 		.then((response) => response.json())
+	// 		.then((data) => setRobotGallery(data))
+	// }, [])
 	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then((response) => response.json())
-			.then((data) => setRobotGallery(data))
+		const fetchData = async () => {
+			const response = await fetch('https://jsonplaceholder.typicode.com/users')
+			const data = await response.json()
+			setRobotGallery(data)
+		}
+		fetchData()
 	}, [])
 
 	return (
